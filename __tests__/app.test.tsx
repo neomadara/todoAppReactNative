fd224a76b852ec8aeae405a6538ebe4f145e6260
Cristian Gutiérrez <instanciageek@gmail.com>
2022-04-08 21:47:34 -0400

    testing: modify app testing to implement axios mock adapter

--- __tests__/app.test.tsx
+++ __tests__/app.test.tsx
@@ -3,31 +3,32 @@ import React from 'react';
 import App from '../App';
 import {render, waitFor} from '@testing-library/react-native';
 import 'whatwg-fetch';
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
 
 let component: any;
+let mock: any;
+const url = 'endpoint.com/todos';
+const responseData = [
+  {_id: '1', completed: true, title: 'task 01'},
+  {_id: '2', completed: true, title: 'task 02'},
+  {_id: '3', completed: true, title: 'task 03'},
+];
 
 describe('<App /> Component', () => {
   beforeEach(() => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () =>
-          Promise.resolve([
-            {_id: '1', completed: true, title: 'task 01'},
-            {_id: '2', completed: true, title: 'task 02'},
-            {_id: '3', completed: true, title: 'task 03'},
-          ]),
-      }),
-    ) as jest.Mock;
+    mock = new MockAdapter(axios);
+    mock.onGet(url).reply(200, responseData);
     component = render(<App />);
   });
   afterEach(() => {
-    global.fetch = jest.fn();
+    mock.restore();
   });
 
   it('it should render a the initial view state', () => {
     expect(component).toBeDefined();
     expect(component.getByTestId('header-component')).toBeDefined();
-    expect(component.queryAllByTestId('input-component').length).toEqual(1);
+    expect(component.queryAllByTestId('input-component').length).toEqual(0);
   });
   it('should load the TODOS from the API successfully', async () => {
     await waitFor(() =>
